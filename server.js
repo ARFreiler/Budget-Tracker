7fecfc482920174a1e0af3f35ed571998bb2e0c0
Ashleigh Freiler <ashleighfreiler@gmail.com>
2021-07-24 09:49:48 -0400

    Added O to Mongo.

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect(process.env.MONGDB_URI || "mongodb://localhost/budget", {
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/budget", {
   useUnifiedTopology: true,
   useCreateIndex: true,
   useNewUrlParser: true,
