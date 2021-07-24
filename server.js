1b0d673cff0449c3d4bbdae793982c9661968a25
Ashleigh Freiler <ashleighfreiler@gmail.com>
2021-07-24 09:33:37 -0400

    Added lines 19&20 to server.js file.

--- server.js
+++ server.js
@@ -16,6 +16,8 @@ app.use(express.json());
 app.use(express.static("public"));
 
 mongoose.connect(process.env.MONGDB_URI || "mongodb://localhost/budget", {
+  useUnifiedTopology: true,
+  useCreateIndex: true,
   useNewUrlParser: true,
   useFindAndModify: false
 });
